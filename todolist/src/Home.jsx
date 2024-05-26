89dfe7350fe507f579b8dc439e31909f3984f730
Koffi GREBE <yvan.grebe@email.com>
2024-05-26 04:10:14 +0100

    first commit

---
+++ todolist/src/Home.jsx
@@ -0,0 +1,74 @@
+// import React from "react";
+import { useEffect, useState } from "react";
+import Create from "./Create";
+import axios from "axios";
+import { BsCircleFill } from "react-icons/bs";
+import { BsFillTrashFill } from "react-icons/bs";
+import { BsFillCheckCircleFill } from "react-icons/bs";
+
+function Home() {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/get")
+      .then((result) => setTodos(result.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleEdit = (id) => {
+    axios
+      .put("http://localhost:3001/update/" + id)
+      .then((result) => {
+        location.reload();
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleDelete = (id) => {
+    axios
+      .delete("http://localhost:3001/delete/" + id)
+      .then((result) => {
+        location.reload();
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="flex flex-col items-center ">
+      <h1>To do list</h1>
+      <Create />
+      <br />
+      {todos.length === 0 ? (
+        <div>
+          <h2>No Record</h2>
+        </div>
+      ) : (
+        todos.map((todo) => (
+          <div className="bg-black text-white h-35px px-2 py-5 mt-2  flex items-center justify-between w-345px">
+            <div
+              className="flex items-center"
+              onClick={() => handleEdit(todo._id)}
+            >
+              {todo.done ? (
+                <BsFillCheckCircleFill className="icon"></BsFillCheckCircleFill>
+              ) : (
+                <BsCircleFill className="icon" />
+              )}
+              <p className={todo.done ? "line-through" : ""}>{todo.task}</p>
+            </div>
+            <div>
+              <span>
+                <BsFillTrashFill
+                  className="icon"
+                  onClick={() => handleDelete(todo._id)}
+                />
+              </span>
+            </div>
+          </div>
+        ))
+      )}
+    </div>
+  );
+}
+
+export default Home;
